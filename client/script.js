e57ba9595b2e11179830e971cca3dbcac2af8136
Pinglei HE <pingleihe@outlook.com>
2024-03-08 10:26:28 +0100

    finished implementing adding new message

--- client/script.js
+++ client/script.js
@@ -1,5 +1,34 @@
 msgs = [
-    { "msg" : "Hello World" },
-    { "msg" : "Blah Blah" },
-    { "msg" : "I love cats" }
+    {"msg": "Hello World"},
+    {"msg": "Blah Blah"},
+    {"msg": "I love cats"}
 ];
+
+const showMessages = (messages) => {
+    // empty the message list
+    const messageList = document.getElementById('message-list');
+    messageList.innerHTML = '';
+
+    // add each message to the list
+    for (const msg of messages) {
+
+        const li = document.createElement('li');
+        li.className = "message-body";
+        li.appendChild(document.createTextNode(msg.msg));
+
+        messageList.appendChild(li);
+    }
+}
+
+const update = showMessages; // alias
+
+document
+    .querySelector("#message-form > button")
+
+    .addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const input = document.getElementById('new-msg').value;
+        msgs.push({"msg": input});
+        update(msgs);
+    })
