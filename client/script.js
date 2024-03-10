05ab390d6c5dead7aea589d7fe47be4ff5415440
Pinglei HE <pingleihe@outlook.com>
2024-03-10 22:49:14 +0100

    3.1 prise en main

--- client/script.js
+++ client/script.js
@@ -1,4 +1,6 @@
-msgs = [
+const SERVER_ADDR = "http://localhost:8080";
+
+DEFAULT_MESSAGES = [
     {"msg": "Hello World"},
     {"msg": "Blah Blah"},
     {"msg": "I love cats"}
@@ -20,10 +22,10 @@ const showMessages = (messages) => {
     }
 }
 
-const update = showMessages; // alias
+const update = () => showMessages(DEFAULT_MESSAGES); // alias
+update();
 
-update(msgs);
-
+// add event listener to the button to append a new message
 document
     .querySelector("#message-form > button")
 
@@ -31,6 +33,44 @@ document
         e.preventDefault();
 
         const input = document.getElementById('new-msg').value;
-        msgs.push({"msg": input});
-        update(msgs);
+        DEFAULT_MESSAGES.push({"msg": input});
+        update(DEFAULT_MESSAGES);
     })
+
+/**
+ * Fetches all messages from the server asynchronously.
+ *
+ * @async
+ * @returns {Promise<string[]>} A promise that resolves with an array of messages in string format.
+ */
+const getAllMessages = async () => {
+    const url = `${SERVER_ADDR}/msg/getAll`;
+    const response = await fetch(url);
+    return await response.json();
+}
+
+/**
+ * Loads all messages from the server and displays them.
+ * If no messages are available, it falls back to the default messages.
+ *
+ * @async
+ * @returns {Promise<void>}
+ */
+const loadMessages = async () => {
+    const messages = await getAllMessages();
+
+    if (messages.length === 0) {
+        // fallback to placeholder messages
+        showMessages(DEFAULT_MESSAGES);
+        return;
+    }
+
+    const firstMessage = messages[0];
+    alert(firstMessage);
+
+    // convert format to re-use the existing showMessages function
+    showMessages(messages.map(msg => ({msg})));
+}
+
+loadMessages()
+    .then(); // explicitly ignore the return value
